371c5fd57651b460f39d399fdd779e663e9cb1bf
husnainisworking <husnainisworking@gmail.com>
2026-02-17 00:19:50 +0500

    feat: add Material Design underline inputs with sliding hover animation on auth pages

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,10 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<div class="relative underline-input border-b-2 border-gray-300 dark:border-gray-600">
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-0  
+px-3 rounded-none outline-none bg-transparent shadow-none
+ focus:ring-0
+  dark:bg-transparent dark:text-white
+transition-colors duration-300
+']) }}>
+</div>
